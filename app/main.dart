675fb8338402b74c2b957df07ff593fab55be0d5
Till Heinze <till-heinze@web.de>
2025-04-24 19:37:52 +0200

    cleanup and moved everything into one project

---
+++ app/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xffe50043)),
+      ),
+      home: MyHomePage(title: 'Viducate'),
+    );
+  }
+
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("go to addMusikPlaylistPage"),
+          ),
+
+          ElevatedButton(
+            onPressed: () {},
+            child: Text("go to addVideoPlaylistPage"),
+          )
+        ],
+      ),
+    );
+  }
+}
